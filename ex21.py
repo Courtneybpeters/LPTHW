3924f1ba9135759016d98c4ff1ad97813238a296
Courtney Peters <courtneybpeters@gmail.com>
2014-12-31 18:29:08 -0600

    Completed ex 21

--- ex21.py
+++ ex21.py
@@ -11,12 +11,25 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
-    print "Dividing %d / %d" (a, b)
+    print "Dividing %d / %d" % (a, b)
     return a / b
 
 print "Let's do some math with just functions!"
 
-age = add(20 + 6)
-height = subtract(78-4)
-weight = multiply(130 * 2)
-iq =
+age = add(30, 5) #35
+height = subtract(78, 4) #74
+weight = multiply(90, 2) #180
+iq = divide(100, 2) #50
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+#Extra credit
+print "Here is a puzzle"
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+
+# The answer is:
+# -4391
+
+print "That becomes: ", what
